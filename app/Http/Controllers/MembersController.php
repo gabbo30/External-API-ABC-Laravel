6ee0a150badd38c51cf2c7e67af52c8cbc71729b
Gabriel Rodriguez <junior.gibson.lp@gmail.com>
2024-09-11 17:30:39 -0600

    First commit

---
+++ app/Http/Controllers/MembersController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class MembersController extends Controller
+{
+    public $api_key;
+    public $api_password;
+    public $club_id = '3754';
+
+    public function __construct()
+    {
+        // Definir las credenciales de la API
+        $this->api_key = 'XXXXXX';
+        $this->api_password = 'XXXXXXXXXXXXXXXXXX';
+    }
+
+    public function members()
+    {
+        $url = "https://api.abcfinancial.com/rest/{$this->club_id}/members/";
+
+        $response = Http::withHeaders([
+            'app_id' => $this->api_key,
+            'app_key' => $this->api_password,
+            'Accept' => 'application/json',
+        ])->get($url);
+
+        if ($response->failed()) {
+            return response()->json(['error' => 'Error en la solicitud a la API'], 500);
+        }
+
+        $data = $response->json();
+
+        if ($data === null || json_last_error() !== JSON_ERROR_NONE) {
+            return response()->json(['error' => 'Error al decodificar la respuesta JSON'], 500);
+        }
+
+        return response()->json($data);
+    }
+}
